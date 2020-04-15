7b395d98fe1b47e87432bd06201b5916600125e5
Enmanuel-Leon <45642674+enmanuel23x@users.noreply.github.com>
2020-04-15 09:48:34 -0400

    Update keysDB.js

--- config/keysDB.js
+++ config/keysDB.js
@@ -3,8 +3,8 @@ module.exports = {
     database:{
 	    host: '10.48.13.154',
 	    port: 3306,
-	    user: 'root',
-	    password : 'userdb',
+	    user: 'server',
+	    password : '123456',
 	    database : 'MAPEO',
 		timezone: "+00:00",
 		connectionLimit: 10,
