251f7aaae91a205e87ef4fc3a50e830a2bb33947
Alejandro <aesbetancourt@outlook.com>
2020-04-21 10:46:44 -0400

    Email updated
    anarvaez mail

--- config/keys.js
+++ config/keys.js
@@ -2,8 +2,8 @@ module.exports = {
     TOKEN:'TL5O159iOxHJL2SV-z4JhUTKclGFuE4cX4E7DjMfWP0',
     CLIENT_ID: 'vdoO8xLDjDFtQIqHvO6UWHNkOIGPEMy18GdHfKzXuaY',
     CLIENT_SECRET: 'lUVyjNQvMJk3QHtzFsUvm-tzkCT7pP0O9aDFnsLW3Oc',
-    EMAIL: 'enma2310@outlook.com',
-    PASS: '23pizzaxnoL',
-    HOST: 'smtp.office365.com',
-    PORT: 587
+    EMAIL: 'anarvaezmalave@gmail.com',
+    PASS: 'Yujuli800',
+    HOST: 'smtp.gmail.com',
+    PORT: 465
 }
