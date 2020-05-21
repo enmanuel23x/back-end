b22f033c27c5bba5d76af97fbc95df99fc790258
Enmanuel-Leon <45642674+enmanuel23x@users.noreply.github.com>
2020-05-21 18:00:12 -0400

    crt deleted

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 //NPM Requires
 const express = require('express');
 const cors = require('cors');
-const fs = require('fs');
-const https = require('https');
-const sameSiteCookieMiddleware = require('express-samesite-default').sameSiteCookieMiddleware;
-//Crt
-var options = {
-    key: fs.readFileSync('./config/private.key'),
-    cert: fs.readFileSync('./config/combined.crt'),
-    requestCert: false,
-    rejectUnauthorized: false
-};
 //Project's own requires
 const config  = require('./config/config').server;
 //Initializations
@@ -21,17 +11,16 @@ const mail = require('./routes/sendEmail')
 const conn_logs = require('./routes/conn_logs')
 //Express Settings
 app.use(cors());
-app.use(sameSiteCookieMiddleware());
 //Express Middlewares
 app.use(express.json()); 
 //Express Routes
-
 //app.use('/auth', auth)
 app.use('/rg', rg)
 app.use('/email',mail)
 app.use('/bd',conn_logs)
 //Start Server
 const PORT = process.env.PORT || config.port;
-var server = https.createServer(options, app).listen(PORT, function(){
+
+app.listen(PORT, function(){
     console.log(`Server running on ${config.host}:${PORT}`)
 });
